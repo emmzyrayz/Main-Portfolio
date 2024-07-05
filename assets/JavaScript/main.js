49c72eb0f770e0b62b049622d31929d612423ea1
Nnamdi Emmanuel Dike <emmanueldike275@gmail.com>
2024-07-05 03:55:19 +0100

    added tutorial class page

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -433,3 +433,21 @@ document.addEventListener("DOMContentLoaded", function () {
   // Run the quote generation function when the DOM is fully loaded
   displayQuote();
 });
+
+
+// faq 
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
